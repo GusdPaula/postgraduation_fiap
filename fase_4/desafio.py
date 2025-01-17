ebdce480b7aaf1afa6fecc29744c80f2c530b083
GusdPaula <108082742+GusdPaula@users.noreply.github.com>
2025-01-17 16:08:17 -0300

    Add files via upload

--- fase_4/desafio.py
+++ fase_4/desafio.py
@@ -8,6 +8,33 @@ import os
 import __main__
 from imblearn.over_sampling import SMOTE
 
+
+st.write('# Preditor de Doença Vascular')
+
+st.write('O questionário abaixo te guiará a preencher as features relavantes para um modelo de predição de doença vascular.')
+
+st.write('#### Nota:')
+st.write('##### Pressão')
+st.write('A pressão sistólica é o valor mais alto que aparece durante uma aferição e está ligada ao movimento de contração do coração. Já a pressão diastólica é o valor mais baixo e está ligada ao relaxamento do coração.')
+st.write('Sua pressão arterial sistólica mede a força do sangue contra as paredes das artérias enquanto seus ventrículos – as duas câmaras inferiores do coração – se comprimem, empurrando o sangue para o resto do corpo.')
+st.write('Sua pressão arterial diastólica mede a força do sangue contra as paredes das artérias enquanto o coração relaxa e os ventrículos podem se encher de sangue.')
+st.write()
+st.write('##### Glicose')
+st.write('A glicose é um carboidrato simples do tipo monossacarídeo e representa a principal fonte de energia dos seres vivos.\nEla pode ser encontrada livre ou combinada com outros tipos de carboidratos.\nNas células, a glicose é a principal fonte de energia, sendo usada no processo de respiração celular. Além de ser um dos principais produtos da fotossíntese.')
+st.markdown("""
+    - Normal: Até 99 mg/dl.
+    - Pré-diabetes (acima do normal): De 110 a 126 mg/dl.
+    - Diabetes (muito acima do normal): Acima de 126 mg/dl.)
+""")
+st.write()
+st.write('##### Colesterol (Total)')
+st.write('O colesterol é um tipo de gordura encontrado no organismo, sendo essencial para o funcionamento das células do cérebro, nervos, músculos, pele e fígado, por exemplo, além de atuar na formação de hormônios e vitamina D')
+st.markdown("""
+    - Normal: Menos de 200 mg/dL
+    - Acima do normal (limítrofe): 200-239 mg/dL
+    - Muito acima do normal (alto): 240 mg/dL ou mais
+""")
+
 # Idade
 st.write("""### Idade""")
 input_idade = float(st.slider('Selecione a sua idade', 18, 100))
@@ -21,11 +48,11 @@ input_genero = input_genero_dict.get(input_genero)
 
 # Altura
 st.write("""### Altura""")
-input_altura = float(st.slider('Selecione a sua altura', 100, 250))
+input_altura = float(st.slider('Selecione a sua altura (em centímetros)', 100, 250))
 
 # Peso
 st.write("""### Peso""")
-input_peso = float(st.slider('Selecione seu peso', 40, 250))
+input_peso = float(st.slider('Selecione seu peso (em kilos)', 40, 250))
 
 # PressaoArterialSistolica
 st.write("""### Pressâo Arterial Sistólica""")
@@ -37,11 +64,11 @@ input_PressaoArterialDiastolica = float(st.slider('Selecione Pressâo Arterial D
 
 # colesterol
 st.write("""### Colesterol""")
-input_colesterol = float(st.slider('Selecione seu colesterol', 1, 3))
+input_colesterol = float(st.slider('Selecione seu colesterol (1: normal, 2: acima do normal, 3: muito acima do normal)', 1, 3))
 
 # glicose
 st.write("""### Glicose""")
-input_glicose = float(st.slider('Selecione sua glicose', 1, 3))
+input_glicose = float(st.slider('Selecione sua glicose (1: normal, 2: acima do normal, 3: muito acima do normal)', 1, 3))
 
 # Fumante
 st.write('### Fumante?')
